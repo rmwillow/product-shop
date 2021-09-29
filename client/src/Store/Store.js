f88e86c888da0caba6e4c97e5a6bb86a3a352284
Rachel McGrath <rachel.mcgrath@degreeanalytics.com>
2021-09-29 09:57:24 -0400

    list products reducer, action, and comonent

---
+++ client/src/Store/Store.js
@@ -0,0 +1,14 @@
+
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { listProducts } from "../Reducers/productReducer";
+
+const rootReducer = combineReducers({
+  productList: listProducts,
+});
+
+const initialState = {};
+
+const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
+
+export default store;
