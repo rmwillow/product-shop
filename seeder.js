5d35eac5d17df4cfe323a7a941dfd00d3acc53b1
Rachel McGrath <rachel.mcgrath@degreeanalytics.com>
2021-09-28 20:28:33 -0400

    Category file added to seeder

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/config.env" });
 //load models
 const User = require("./models/User");
 const Product = require("./models/Product");
+const Category = require("./models/Category");
 // Connect to DB
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -28,12 +29,17 @@ const products = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/product.json`, "utf-8")
 );
 
+const categories = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/category.json`, "utf-8")
+);
+
 //Import into DB
 
 const importData = async () => {
   try {
     await User.create(users);
     await Product.create(products);
+    await Category.create(categories);
     console.log(`Data Imported`.green.inverse);
     process.exit();
   } catch (error) {
@@ -45,6 +51,7 @@ const deleteData = async () => {
   try {
     await User.deleteMany();
     await Product.deleteMany();
+    await Category.deleteMany();
     console.log("Data Destroy".red.inverse);
     process.exit();
   } catch (error) {
