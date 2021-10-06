8ff89be59759174e82e5d9c49411c9d7fb8ade6e
Rachel McGrath <rachel.mcgrath@degreeanalytics.com>
2021-10-05 19:54:12 -0500

    product reducer added

--- client/src/Constants/productConstants.js
+++ client/src/Constants/productConstants.js
@@ -1,3 +1,50 @@
 export const PRODUCT_FETCH_START = "PRODUCT_FETCH_START";
 export const PRODUCT_FETCH_SUCCESS = "PRODUCT_FETCH_SUCCESS";
-export const PRODUCT_FETCH_ERROR = "PRODUCT_FETCH_ERROR";
+export const PRODUCT_FETCH_FAIL = "PRODUCT_FETCH_FAIL";
+
+import * as productConstants from "../Constants/productConstants";
+
+export const listProducts = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case productConstants.PRODUCTLIST_FETCH_START:
+      return {
+        loading: true,
+        products: [],
+      };
+    case productConstants.PRODUCTLIST_FETCH_SUCCESS:
+      return {
+        products: action.payload.productList,
+        count: action.payload.totalProduct,
+        success: true,
+      };
+    case productConstants.PRODUCTLIST_FETCH_ERROR:
+      return {
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const Product = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case productConstants.PRODUCT_FETCH_START:
+      return {
+        loading: true,
+        product: [],
+      };
+    case productConstants.PRODUCT_FETCH_SUCCESS:
+      return {
+        product: action.payload,
+        success: true,
+      };
+    case productConstants.PRODUCT_FETCH_FAIL:
+      return {
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
