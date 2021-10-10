c4f856796786f219bf4b4625b8a3ce56bd9f4af6
Rachel McGrath <rachel.mcgrath@degreeanalytics.com>
2021-10-10 17:27:45 -0500

    landing and item components

--- client/src/components/Item/Item.js
+++ client/src/components/Item/Item.js
@@ -1,55 +1,104 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { CardActionArea, CardMedia, Typography, CardContent, Card } from "@material-ui/core";
 import { motion } from "framer-motion";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { addToCart } from "../../store/actions/orderActions";
+import { setSelect } from "../../store/actions/products";
 const useStyles = makeStyles({
     root: {
+        backgroundColor: "#f6f6f6",
         maxWidth: 345,
     },
     media: {
-        height: 140,
+        height: 325,
     },
+    actionArea: {
+        "&:hover $focusHighlight": {
+            opacity: 0,
+        },
+    },
+    focusHighlight: {},
 });
+//============= framer =================
+const transition = { duration: 0.5, ease: [0.43, 0.13, 0.23, 0.96] };
+
+const thumbnailVariants = {
+    initial: { scale: 0.9, opacity: 0 },
+    enter: { scale: 1, opacity: 1, transition },
+    exit: {
+        scale: 0.5,
+        opacity: 0,
+        transition: { duration: 1.5, ...transition },
+    },
+};
+
+const frameVariants = {
+    hover: { scale: 0.95 },
+};
+
+const imageVariants = {
+    hover: { scale: 1.1 },
+};
+
+// ===================
 const SimpleCard = (props) => {
     const classes = useStyles();
+    console.log(props.data._id);
 
     return (
-        <motion.div initial={{ opacity: 0 }} animate={{ y: -60, opacity: 1 }} transition={{ duration: 1 }}>
-            <Card className={classes.root}>
-                <CardActionArea>
-                    <CardMedia
-                        className={classes.media}
-                        image="/static/images/cards/contemplative-reptile.jpg"
-                        title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {props.name}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                            across all continents except Antarctica
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                        Share
-                    </Button>
-                    <Button size="small" color="primary">
-                        Learn More
-                    </Button>
-                </CardActions>
-            </Card>
+        <motion.div variants={thumbnailVariants}>
+            <motion.div
+                className="frame"
+                whileHover="hover"
+                variants={frameVariants}
+                transition={transition}
+            >
+                <Card className={classes.root} elevation={0}>
+                    <CardActionArea
+                        disableRipple
+                        disableTouchRipple
+                        classes={{
+                            root: classes.actionArea,
+                            focusHighlight: classes.focusHighlight,
+                        }}
+                        onClick={() => props.setSelect(props.data._id)}
+                        component={Link}
+                        to="/select"
+                    >
+                        <motion.div variants={imageVariants} transition={transition}>
+                            <CardMedia
+                                className={classes.media}
+                                image={props.data.imageLink}
+                                title="Contemplative Reptile"
+                            />
+                        </motion.div>
+
+                        <CardContent style={{ textAlign: "center" }}>
+                            <Typography style={{ fontSize: "1.8rem" }} variant="h3" component="h3">
+                                {props.data.name}
+                            </Typography>
+                            <Typography
+                                variant="subtitle1"
+                                style={{ fontSize: "1.5rem" }}
+                                component="p"
+                            >
+                                $ {props.data.price}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </motion.div>
         </motion.div>
-
     );
-}
+};
 
-export default SimpleCard;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addToCart: (data) => dispatch(addToCart(data)),
+        setSelect: (id) => dispatch(setSelect(id)),
+    };
+};
+
+export default connect(null, mapDispatchToProps)(SimpleCard);
--- client/src/components/Item/ItemGrid/ItemGrid.js
+++ client/src/components/Item/ItemGrid/ItemGrid.js
@@ -1,31 +1,60 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Item from "../Item"
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Item from "../Item";
+import { motion } from "framer-motion";
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
-    }
+    },
 }));
+// // framer motion ========
+// const transition = { duration: 0.5, ease: [0.43, 0.13, 0.23, 0.96] };
+// // ======================
 const ItemGrid = (props) => {
     const classes = useStyles();
     return (
-        <div className={classes.root}>
+        <motion.div
+            className={classes.root}
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            variants={{ exit: { transition: { staggerChildren: 0.1 } } }}
+        >
             <Grid container spacing={3}>
-                {
-                    props.items ?
-                        props.items.map(function (x) {
-                            if (x.category != null) {
-                                return <Grid item xs={6} sm={4} key={x.id} >
-                                    <Item name={x.name} />
-                                </Grid>
+                {props.items != null ? (
+                    // eslint-disable-next-line
+                    props.items.map((x) => {
+                        if (props.brand != null) {
+                            if (
+                                x.brand === props.brand &&
+                                x.price >= Number(props.minPrice) &&
+                                x.price <= Number(props.maxPrice)
+                            ) {
+                                return (
+                                    <Grid item xs={12} sm={4} key={x._id}>
+                                        <Item data={x} />
+                                    </Grid>
+                                );
                             }
-                        })
-                        :
-                        <p>loading!!!!!!!!</p>
-                }
+                        } else {
+                            if (
+                                x.price >= Number(props.minPrice) &&
+                                x.price <= Number(props.maxPrice)
+                            ) {
+                                return (
+                                    <Grid item xs={12} sm={4} key={x._id}>
+                                        <Item data={x} />
+                                    </Grid>
+                                );
+                            }
+                        }
+                    })
+                ) : (
+                    <p>Please Select any category</p>
+                )}
             </Grid>
-        </div>
+        </motion.div>
     );
-}
+};
 export default ItemGrid;
