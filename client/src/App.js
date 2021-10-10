40dc641b9fc24a2bfb5caa9916cbd572fd26bc85
Rachel McGrath <rachel.mcgrath@degreeanalytics.com>
2021-10-10 16:20:24 -0500

    categories

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,12 @@ import Home from "./components/Home/Home";
 import Products from "./containers/Products/Products";
 import { Switch, Route } from "react-router-dom"
 import { connect } from "react-redux";
-// import * as actionTypes from "./store/actions/actionTypes"
+//* import * as actionTypes from "./store/actions/actionTypes"
 import * as actions from "./store/actions/authActions";
 import './App.css';
+import theme from "./theme";
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
 //let width = window.innerWidth;
 class App extends Component {
   componentDidMount() {
@@ -13,17 +16,17 @@ class App extends Component {
   }
   render() {
     return (
-      <div style={{
-        height: "100%"
-      }}>
-        <Switch>
-          <Route path="/products" component={Products} />
-          <Route path="/" component={Home} />
-        </Switch>
-        {/* <Link to="/">Home</Link>
-        
-        <Link to="/products">Products</Link> */}
-      </div>
+      <ThemeProvider theme={theme} >
+        <CssBaseline />
+        <div style={{
+          height: "100%"
+        }}>
+          <Switch>
+            <Route path="/products" component={Products} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
+      </ThemeProvider>
     )
   };
 }
@@ -41,7 +44,7 @@ const mapDispatchToProps = (dispatch) => {
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
 
-{/* 
+/*{/*
 import Items from "./components/list";
 import Register from "./components/Register";
 import Login from "./components/Login";
@@ -54,4 +57,4 @@ import Login from "./components/Login";
         }} onClick={this.props.logOut}>LOGOUT</button> : <React.Fragment>
             <Register />
             <Login />
-          </React.Fragment>} */}
+          </React.Fragment>} */
