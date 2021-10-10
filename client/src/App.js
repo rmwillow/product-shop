646387a20ba94dbc29cc7591e7a849ebf2950399
Rachel McGrath <rachel.mcgrath@degreeanalytics.com>
2021-10-10 16:47:41 -0500

    ccompiled index

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,50 @@
-import React, { Component } from 'react';
+import React from "react";
 import Home from "./components/Home/Home";
 import Products from "./containers/Products/Products";
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route, useLocation, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-//* import * as actionTypes from "./store/actions/actionTypes"
+import { AnimatePresence } from "framer-motion";
 import * as actions from "./store/actions/authActions";
-import './App.css';
+import "./App.css";
 import theme from "./theme";
-import { ThemeProvider } from '@material-ui/core/styles'
-import CssBaseline from '@material-ui/core/CssBaseline'
-//let width = window.innerWidth;
-class App extends Component {
-  componentDidMount() {
-    this.props.loadUser();
-  }
-  render() {
-    return (
-      <ThemeProvider theme={theme} >
-        <CssBaseline />
-        <div style={{
-          height: "100%"
-        }}>
-          <Switch>
-            <Route path="/products" component={Products} />
-            <Route path="/" component={Home} />
-          </Switch>
-        </div>
-      </ThemeProvider>
-    )
-  };
-}
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Selected from "./containers/Selected/Selected";
 
-const mapStateToProps = (state) => {
-  return { isAuth: state.auth.isAuth }
+//let width = window.innerWidth;
+function App(props) {
+    const location = useLocation();
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div
+                style={{
+                    height: "100%",
+                }}
+            >
+                {/* <AnimatePresence exitBeforeEnter>  key={location.pathname} */}
+                <AnimatePresence exitBeforeEnter>
+                    <Switch location={location} key={location.pathname}>
+                        <Route path="/products" component={Products} />
+                        <Route path="/select" component={Selected} />
+                        <Route path="/" component={Home} />
+                    </Switch>
+                </AnimatePresence>
+                {/* </AnimatePresence> */}
+            </div>
+        </ThemeProvider>
+    );
 }
+//backdrop filter-css \\ half pattern and half blur
+const mapStateToProps = (state) => {
+    return { isAuth: state.auth.isAuth };
+};
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    loadUser: () => dispatch(actions.loadUser()),
-    logOut: () => dispatch(actions.logOut())
-  }
-}
+    return {
+        loadUser: () => dispatch(actions.loadUser()),
+        logOut: () => dispatch(actions.logOut()),
+    };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-/*{/*
-import Items from "./components/list";
-import Register from "./components/Register";
-import Login from "./components/Login";
-<Items />
-        {this.props.isAuth ? <button style={{
-          backgroundColor: "red",
-          border: "3px solid black",
-          color: "white",
-          borderRadius: "5px"
-        }} onClick={this.props.logOut}>LOGOUT</button> : <React.Fragment>
-            <Register />
-            <Login />
-          </React.Fragment>} */
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
