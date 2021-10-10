64f8df741afbe3206322b912da90d148c9d40c6e
Rachel McGrath <rachel.mcgrath@degreeanalytics.com>
2021-10-10 17:06:32 -0500

    product and order reducers

--- client/src/store/Reducers/authReducer.js
+++ client/src/store/Reducers/authReducer.js
@@ -1,49 +1,62 @@
 import * as actionTypes from "../actions/actionTypes";
 
 const initState = {
-    token: localStorage.getItem('token'),
+    token: localStorage.getItem("token"),
     isAuth: null,
+    showAuthModal: false,
+
     isLoading: false,
-    user: null
-}
+    user: null,
+    isLogin: true,
+};
 
 export default function (state = initState, action) {
     switch (action.type) {
         case actionTypes.USER_LOADING:
             return {
                 ...state,
-                isLoading: true
-            }
-
+                isLoading: true,
+            };
+        case actionTypes.TOGGLE_AUTH_MODAL:
+            return {
+                ...state,
+                showAuthModal: !state.showAuthModal,
+            };
         case actionTypes.USER_LOADED:
             return {
                 ...state,
                 isLoading: false,
                 isAuth: true,
-                user: action.payload
-            }
+                user: action.payload,
+            };
 
         case actionTypes.LOGIN_SUCCESS:
         case actionTypes.REGISTER_SUCCESS:
-            localStorage.setItem('token', action.payload.token)
+            localStorage.setItem("token", action.payload.token);
             return {
                 ...state,
                 ...action.payload,
                 isAuth: true,
-                isLoading: false
-            }
+                isLoading: false,
+                showAuthModal: false,
+            };
         case actionTypes.AUTH_ERROR:
         case actionTypes.LOGIN_FAIL:
         case actionTypes.LOGOUT_SUCCESS:
         case actionTypes.REGISTER_FAIL:
-            localStorage.removeItem('token');
+            localStorage.removeItem("token");
             return {
                 ...state,
                 token: null,
                 user: null,
                 isAuth: null,
-                isLoading: false
-            }
+                isLoading: false,
+            };
+        case actionTypes.CHANGE_AUTH_MODE:
+            return {
+                ...state,
+                isLogin: !state.isLogin,
+            };
         default:
             return state;
     }
