2facfef60451b0a2c73988feac1fcad85061a40c
Rachel McGrath <rachel.mcgrath@degreeanalytics.com>
2021-10-10 17:23:56 -0500

    home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,86 +1,128 @@
-import React, { Component } from 'react';
-import "./Home.css"
+import React, { Component } from "react";
+import { motion } from "framer-motion";
+import "./Home.css";
+import { connect } from "react-redux";
+import { getItem } from "../../store/actions/products";
 var classNames = require("classnames");
 class Home extends Component {
     state = {
         fashionOpen: false,
         shoesOpen: false,
         aceesOpen: false,
-        ElectronicsOpen: false
-    }
+        ElectronicsOpen: false,
+    };
+    nav = (id) => {
+        this.props.getItem(id);
+        this.props.history.push("/products");
+        console.log("pushed!");
+    };
     toggleFashionOpen = () => {
         this.setState({
-            fashionOpen: !this.state.fashionOpen
-        })
-    }
+            fashionOpen: !this.state.fashionOpen,
+        });
+    };
     toggleShoesOpen = () => {
         this.setState({
-            shoesOpen: !this.state.shoesOpen
-        })
-    }
+            shoesOpen: !this.state.shoesOpen,
+        });
+    };
     toggleAceesOpen = () => {
         this.setState({
-            aceesOpen: !this.state.aceesOpen
-        })
-    }
+            aceesOpen: !this.state.aceesOpen,
+        });
+    };
     toggleElectronicsOpen = () => {
         this.setState({
-            ElectronicsOpen: !this.state.ElectronicsOpen
-        })
-    }
+            ElectronicsOpen: !this.state.ElectronicsOpen,
+        });
+    };
+
     render() {
+        const home = {
+            animate: {
+                opacity: 1,
+                transition: {
+                    duration: 3,
+                },
+            },
+            exit: {
+                opacity: 0,
+                transition: {
+                    duration: 1,
+                },
+            },
+        };
         return (
-            <div className="table">
+            <motion.div
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                variants={home}
+                className="table"
+            >
                 <div
                     style={{
-                        backgroundPosition: 'center',
+                        cursor: "pointer",
+                        backgroundPosition: "center -5rem",
                         backgroundRepeat: "no-repeat",
-                        backgroundImage: 'url(" https://images.unsplash.com/photo-1505734169265-a86113baa6c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80")'
+                        backgroundImage:
+                            'url(" https://images.unsplash.com/photo-1505734169265-a86113baa6c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80")',
                     }}
-                    className={classNames("table__cell", { "active": this.state.fashionOpen })}
+                    className={classNames("table__cell", { active: this.state.fashionOpen })}
                     onMouseEnter={this.toggleFashionOpen}
                     onMouseLeave={this.toggleFashionOpen}
-                >
-                    <span><h1>A</h1></span>
-                </div>
-                <div
-                    style={{
-                        backgroundPosition: 'center',
-                        backgroundRepeat: "no-repeat",
-                        backgroundImage: 'url(" https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80")'
-                    }}
-                    className={classNames("table__cell", { "active": this.state.shoesOpen })}
-                    onMouseEnter={this.toggleShoesOpen}
-                    onMouseLeave={this.toggleShoesOpen}>
-                    <span><h1>B</h1></span>
-                </div>
-                <div
-                    style={{
-                        backgroundPosition: 'center',
-                        backgroundRepeat: "no-repeat",
-                        backgroundImage: 'url(" https://images.unsplash.com/photo-1523206489230-c012c64b2b48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80")'
-                    }}
-                    className={classNames("table__cell", { "active": this.state.aceesOpen })}
-                    onMouseEnter={this.toggleAceesOpen}
-                    onMouseLeave={this.toggleAceesOpen}>
-                    <span><h1>C</h1></span>
-                </div>
-                <div
-                    style={{
+                    onClick={() => this.nav("clothing")}
+                ></div>
 
+                <div
+                    style={{
+                        cursor: "pointer",
+                        backgroundPosition: "center",
                         backgroundRepeat: "no-repeat",
-                        backgroundImage: 'url(" https://images.unsplash.com/3/www.madebyvadim.com.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1061&q=80")'
+                        backgroundImage:
+                            'url(" https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80")',
                     }}
-                    className={classNames("table__cell", { "active": this.state.ElectronicsOpen })}
+                    className={classNames("table__cell", { active: this.state.shoesOpen })}
+                    onMouseEnter={this.toggleShoesOpen}
+                    onMouseLeave={this.toggleShoesOpen}
+                    onClick={() => this.nav("shoe")}
+                ></div>
+
+                <div
+                    style={{
+                        cursor: "pointer",
+                        backgroundPosition: "center",
+                        backgroundRepeat: "no-repeat",
+                        backgroundImage:
+                            'url(" https://images.unsplash.com/photo-1523206489230-c012c64b2b48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80")',
+                    }}
+                    className={classNames("table__cell", { active: this.state.aceesOpen })}
+                    onMouseEnter={this.toggleAceesOpen}
+                    onMouseLeave={this.toggleAceesOpen}
+                    onClick={() => this.nav("phone")}
+                ></div>
+                <div
+                    style={{
+                        cursor: "pointer",
+                        backgroundPosition: "-12rem",
+                        backgroundRepeat: "no-repeat",
+                        backgroundImage:
+                            'url(" https://images.unsplash.com/flagged/photo-1592500410522-c194f4fa21b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80")',
+                    }}
+                    className={classNames("table__cell", { active: this.state.ElectronicsOpen })}
                     onMouseEnter={this.toggleElectronicsOpen}
-                    onMouseLeave={this.toggleElectronicsOpen}>
-                    <span><h1>D</h1></span>
-                </div>
-            </div>
-        )
+                    onMouseLeave={this.toggleElectronicsOpen}
+                    onClick={() => this.nav("accessories")}
+                ></div>
+            </motion.div>
+        );
     }
 }
 
-export default Home;
-/* onMouseEnter={this.toggleFashionOpen}
-                    onMouseLeave={this.toggleFashionOpen} */
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getItem: (id) => dispatch(getItem(id)),
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Home);
