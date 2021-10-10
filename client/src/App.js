d78aee11febe7235cb7d465e8a2294f428d889fa
Rachel McGrath <rachel.mcgrath@degreeanalytics.com>
2021-10-09 19:34:15 -0500

    adding  matierl ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Home from "./components/Home/Home";
-import Products from "./components/Products/Products";
-import { Switch, Route, Link } from "react-router-dom"
+import Products from "./containers/Products/Products";
+import { Switch, Route } from "react-router-dom"
 import { connect } from "react-redux";
-import * as actionTypes from "./store/actions/actionTypes"
+// import * as actionTypes from "./store/actions/actionTypes"
 import * as actions from "./store/actions/authActions";
 import './App.css';
 //let width = window.innerWidth;
