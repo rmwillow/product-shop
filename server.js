579a93068efc0506eda219b4333bb739a5d9acdd
Rachel McGrath <rachel.mcgrath@degreeanalytics.com>
2021-10-14 08:58:40 -0500

    testing heroku data

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 app.use(express.json());
 const items = require("./routes/api/items"); //* all routes are here
 const Users = require("./routes/api/Users"); //* all routes are here
-// mongoose.set("useNewUrlParser", true);
-// mongoose.set("useFindAndModify", false);
-// mongoose.set("useCreateIndex", true);
-// mongoose.set("useUnifiedTopology", true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 mongoose
-    .connect('mongodb://localhost/Product-shop')
+    .connect(process.env.MONGODB_URI || 'mongodb://localhost/Product-shop')
     // .connect("MONGODB_URI")
     .then(() => console.log("Connected to DataBase"))
     .catch((err) => console.log("error: ", err.message));
